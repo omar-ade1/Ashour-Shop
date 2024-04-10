8b58320381508277c11b9dbe6d0387160831fbf6
omar adel <omar50001000@gmail.com>
2024-04-10 20:32:46 +0300

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import reactRedux from 'react-redux'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    reactRedux() // استخدام react-redux ك plugin
-  ]
+  plugins: [react()],
 })
